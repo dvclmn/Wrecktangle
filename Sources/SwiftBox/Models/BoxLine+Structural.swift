ed1b10defae45aca5e5ba083f20aacb0fc828957
Dave Coleman <dave@dvclmn.com>
2024-09-13 17:06:32 +0200

    Tidies files, removes some old code

---
+++ Sources/SwiftBox/Models/BoxLine+Structural.swift
@@ -0,0 +1,28 @@
+//
+//  BoxLine+Structural.swift
+//  SwiftBox
+//
+//  Created by Dave Coleman on 13/9/2024.
+//
+
+
+
+public extension SwiftBox {
+
+  struct StructuralContent: LineContent {
+    public let rawContent: String
+    public let type: LineType = .structural
+    let repeatingPattern: BoxPart
+    
+    init(repeatingPattern: BoxPart) {
+      self.repeatingPattern = repeatingPattern
+      self.rawContent = repeatingPattern.content.map { String($0) }.joined(separator: "\n")
+    }
+    
+    func render(width: Int, trimMethod: TrimMethod = .leaveSpace) -> [[Character]] {
+      return SwiftBox.repeatHorizontally(repeatingPattern, toWidth: width, trimMethod: trimMethod)
+    }
+  }
+
+
+}
--- Sources/SwiftBox/Models/BoxLine.swift
+++ Sources/SwiftBox/Models/BoxLine.swift
@@ -16,24 +16,7 @@ public extension SwiftBox {
     var rawContent: String { get }
     var type: LineType { get }
   }
-  
-  struct StructuralContent: LineContent {
-    public let rawContent: String
-    public let type: LineType = .structural
-    let repeatingPattern: BoxPart
-    
-    init(repeatingPattern: BoxPart) {
-      self.repeatingPattern = repeatingPattern
-      self.rawContent = repeatingPattern.content.map { String($0) }.joined(separator: "\n")
-    }
-    
-    func render(width: Int, trimMethod: TrimMethod = .leaveSpace) -> [[Character]] {
-      return SwiftBox.repeatHorizontally(repeatingPattern, toWidth: width, trimMethod: trimMethod)
-    }
-  }
-  
-  
- 
+
   struct BoxLine {
     let content: LineContent
     let leadingCap: BoxPart
