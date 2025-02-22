7d293775249b6e85838cd7c37f9aa5c8e8a4192c
Dave Coleman <dave@dvclmn.com>
2025-02-22 18:13:37 +0100

    Minor updates

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "Wrecktangle",
   platforms: [
-    .iOS("16.0"),
+    .iOS("17.0"),
     .macOS("14.0")
   ],
   products: [
@@ -18,7 +18,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/dvclmn/Collection", branch: "main"),
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
 
   ],
   targets: [
